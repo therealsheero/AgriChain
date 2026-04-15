2b0b6bf847e07bdb0279e674d4f555bac50636a6
Shreeya Pandey <131512383+therealsheero@users.noreply.github.com>
2026-04-15 15:00:50 +0530

    Clean up comments in deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,10 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Get the ContractFactory for "Report"
   const ReportFactory = await hre.ethers.getContractFactory("Report");
   
-  // Deploy the contract
   const Report = await ReportFactory.deploy();
-
-  // Wait for the deployment to be mined
   await Report.deployed();
-
-  // Log the address of the deployed contract
   console.log("Deployed contract address:", Report.address);
 }
 
